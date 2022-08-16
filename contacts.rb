9dbd21f4909459fb9d963318341630b1eb35fafa
Maurice <mauricenganga41@gmail.com>
2022-08-16 06:19:55 +0300

    Done.

--- contacts.rb
+++ contacts.rb
@@ -19,7 +19,19 @@ end
 
 def remove_strawberry(contacts)
   # your code here!
+  contacts.each do |person, contact_detail_hash|
+
+    if person == "Freddy Mercury"
+      contact_detail_hash.each do |attribute, value|
+        if attribute == :favorite_ice_cream_flavors
+          value.delete_if{|flavour| flavour == "strawberry"}
+        end
+      end
+    end
+  end
 end
+puts remove_strawberry(contacts)
 
 # print the output to the terminal for inspection
 pp remove_strawberry(contacts)
+
